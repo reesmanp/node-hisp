7b439c76f539827c7cac10bfb097fb1b606a6620
Paul Reesman <paul.reesman@seattlechildrens.org>
2017-09-07 10:12:06 -0700

    Added start and stop scripts; Assumes YARN installed

---
+++ bin/stop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+PIDFILE="../logs/hisp.pid"
+if [ -e $PIDFILE ]; then
+  kill -9 $(<"$PIDFILE")
+  rm -f $PIDFILE
+  echo '*****************'
+  echo 'Node HISP Stopped'
+  echo '*****************'
+else
+  echo '*********************'
+  echo 'Node HISP Not Running'
+  echo '*********************'
+fi
+
